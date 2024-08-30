6094dde139fdf59fc946d5706784cb47c4280d82
nildinho <nildinho.anahi@hotmail.com>
2024-08-30 15:31:46 -0300

    projeto saraiva

--- carrinho/src/routes/carrinho/carrinho.js
+++ carrinho/src/routes/carrinho/carrinho.js
@@ -1,14 +1,35 @@
 const express = require("express")
-const route_carrinho = express.Router()
+const router_carrinho = express.Router()
 const data = require("../../database/config.js")
 
-route_carrinho.get("/listar",(req,res)=>{
-    data.query("select * from carrinho",(error, dados)=>{
-        if(error){
-            return res.status(500).send({msg:"Erro ao carregar dados"})
+router_carrinho.get("/listar", (req, res) => {
+    data.query("select foto.foto1,titulo.nometitulo,titulo.autor,carrinho.quantidade,preco.precoatual,titulo.idtitulo,carrinho.total from saraivalivrodb.fotos foto inner join saraivalivrodb.titulos titulo on foto.idfotos=titulo.idfoto inner join saraivacarrinhodb.carrinho carrinho on titulo.idtitulo=carrinho.idproduto inner join saraivalivrodb.precos preco on preco.idpreco = titulo.idpreco;",(error, dados)=> {
+        if(error) {
+           res.status(500).send({msg: "Erro ao carregar os dados"+error})
         }
-        res.status(200).send({msg:"Ok",payload:dados})
+            res.status(200).send({msg: "Ok", payload:dados})
     })
 })
 
-module.exports = route_carrinho
+router_carrinho.get("/listar/:id", (req, res) => {
+    data.query("select foto.foto1,titulo.nometitulo,titulo.autor,carrinho.quantidade,preco.precoatual,titulo.idtitulo,carrinho.total from saraivalivrodb.fotos foto inner join saraivalivrodb.titulos titulo on foto.idfotos=titulo.idfoto inner join saraivacarrinhodb.carrinho carrinho on titulo.idtitulo=carrinho.idproduto inner join saraivalivrodb.precos preco on preco.idpreco = titulo.idpreco where carrinho.idusuario=1;",req.params.id, (error, dados) => {
+        if(error) {
+            res.status(500).send({msg: "Erro ao carregar os dados"+error})
+        }
+            res.status(200).send({msg: "Ok", payload:dados})
+    })
+})
+
+router_carrinho.post("/inserir", (req, res) => {
+    const dados = req.body
+    data.query("insert into carrinho set ?", [dados], (error, dados) => {
+        if(error) {
+            res.status(500).send({msg: "Erro ao colocar no carrinho"})
+        } else {
+            res.status(200).send({msg: "Ok", payload:dados})
+        }
+    })
+})
+
+
+module.exports = router_carrinho
